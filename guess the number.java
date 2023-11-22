b2aa82d305b3a55918b493750f8e1a2f6e01b492
Rajath0810 <150284699+Rajath0810@users.noreply.github.com>
2023-11-22 18:09:47 +0530

    Add files via upload

---
+++ guess the number.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+public class GFG {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int numberToGuess = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+        int userGuess;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have selected a number between " + lowerBound + " and " + upperBound + ". Try to guess it!");
+
+        do {
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+            attempts++;
+
+            if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+            }
+        } while (userGuess != numberToGuess);
+
+        scanner.close();
+    }
+}
